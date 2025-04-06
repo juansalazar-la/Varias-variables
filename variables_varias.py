4408fb7367d3976c8edd09c8252d9e7b5572f08e
unknown <jusalazarla@unal.edu.co>
2025-04-05 22:07:58 -0500

    Primer commit: archivo de variables

---
+++ variables_varias.py
@@ -0,0 +1,19 @@
+##Esto es un comentario
+#python lenguaje interpretado
+
+#string
+#int
+#float
+#boolean
+
+var= "Esto es un string"
+int= 1
+float= 1.56
+boolean= 4<5
+
+
+print(var)
+print(int)
+print(float)
+print(boolean)
+
